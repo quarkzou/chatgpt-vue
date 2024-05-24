5ccb47f10b758f19d8c0d8d7fd36999c0ab9a350
quarkzou <quarksoft@live.cn>
2024-05-24 09:19:19 +0800

    识别回车键发起对话操作。

--- src/views/home.vue
+++ src/views/home.vue
@@ -44,6 +44,7 @@
             placeholder="请输入您的问题..."
             v-model="messageContent"
             @input="adjustHeight"
+            @keyup.enter="sendOrSave"
         />
         <button class="btn" :disabled="isTalking" @click="sendOrSave()">
           发送
